d524ca5ce9a10e6bb9cbb15d0da7eab7909f36be
yangchen <gyachungriom@gmail.com>
2022-08-03 19:35:22 +0200

    image changed

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+import pathlib
+import uuid
+
+
+def restaurant_image_upload_handler(instance, filename):
+    fpath = pathlib.path(filename)
+    new_fname = str(uuid.uuid1())
+    return f"images/{new_fname}{fpath.suffix}"
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=150)
@@ -9,8 +17,8 @@ class Restaurant(models.Model):
     uber = models.CharField(max_length=150, blank=True)
     link = models.CharField(max_length=150, blank=True)
     fork = models.CharField(max_length=150, blank=True)
-    image= models.FileField(upload_to='media/images', blank=True)
-    
+    image= models.ImageField(upload_to='images', blank=True)
+
     def __str__(self):
         return self.name
 
@@ -21,8 +29,7 @@ class Boutique(models.Model):
     Zip_code = models.CharField(max_length=5, default="75001")
     phone = models.CharField(max_length=15)
     link = models.CharField(max_length=200, blank=True)
-    image= models.FileField(upload_to='media/images', blank=True)
-
+    image= models.ImageField(upload_to='images', blank=True)
     def __str__(self):
         return self.name
 
@@ -34,7 +41,7 @@ class Ngo(models.Model):
     phone = models.CharField(max_length=15)
     link = models.CharField(max_length=200, blank=True)
     type = models.CharField(max_length=200, blank=True)
-    image= models.FileField(upload_to='media/images', blank=True)
+    image= models.ImageField(upload_to='images', blank=True)
 
 # class Contact(models.Model):
 #     firstname = models.CharField(max_length=50 )
